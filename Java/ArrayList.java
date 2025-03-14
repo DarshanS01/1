f6f82b9630bcf6b32822d8c3d4acd3a17e034270
Darshan <darshandarshu7520@gmail.com>
2025-03-15 01:02:32 +0530

    a

---
+++ Java/ArrayList.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> arrList = new ArrayList<>();
+        while (true) 
+        {
+            int num = sc.nextInt();
+            if (num < 0)
+            {
+                break; 
+            }
+            arrList.add(num); 
+        }
+        for (int i = arrList.size() - 1; i >= 0; i--) {
+            System.out.print(arrList.get(i) + " ");
+        }
+    }
+}
