ecc045de68100700db86bbffdaaa3a7bb2485cd7
DarshanS01 <darshandarshu7520@gmail.com>
2025-06-22 23:43:16 +0530

    Update input and Output.js

--- JavaScript/JS introduction/input and Output.js
+++ JavaScript/JS introduction/input and Output.js
@@ -1,5 +1,4 @@
 // Please take input and print output to standard input/output (stdin/stdout)
 // DO NOT USE ARGUMENTS FOR INPUTS
-// YOUR CODE GOES HERE
 var a = readLine();
 console.log("Hello "+a);
